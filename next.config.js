e0c2886dd0fd6b8f187f1dbd7a248a12afd70f89
Jaiharishan <downdkar@gmail.com>
2021-12-30 18:39:46 +0530

    config changed

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     env: {
-        customKey: 'my-value',
-        CLIENTID: 'e2656f8c310e47df98178197a6e8e63e',
-        CLIENTSECRET: '6ccdc3268ee54c42b45a2c5932a86594',
-        NEXTAUTH_URL: 'http://localhost:3000',
-        JWT_SECRET: 'someSecret'
+        customKey: 'YOUR CUSTOM KEY',
+        CLIENTID: "YOUR CLIENTID",
+        CLIENTSECRET: "YOUR CLIENTSECRET",
+        NEXTAUTH_URL: "YOUR PORT",
+        JWT_SECRET: 'YOUR JWT SECRET'
     },
   }
